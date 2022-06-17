c5299b19e18418d910503d9699a50ac459811ad3
Deckloins <trilaf12345@gmail.com>
2022-06-17 16:24:03 -0400

    Timer fixed !

--- sketch.js
+++ sketch.js
@@ -4,13 +4,10 @@ Variables
 
 */
 let gameIsPlaying = false;
-//const canvas = document.getElementById('main')
-let x = 0;
 let playerSpeed = 4;
-let score = 0;
-//let timer = 10 // I'll try to make the code more clear for the time/timer variable 
 var time = 10
-let timerPosition = 340;
+let timerPosition = 310;
+let score = 0;
 let scorePosition = 340
 
 function setup() {
@@ -26,17 +23,17 @@ function setup() {
   pointY = 300;
   pointSize = 10;
 
-  
-
 }
 
 function draw() {
-  time -= 1/60
-  nf(time, 2,0)
+
+  didPlayerMove();
   IsPlayerTouching();
   alignTimerText();
+  gameOver();
+  
   if (gameIsPlaying === true) {
-    setInterval(countDown, 1000)
+
     background(100);
     //Create the points
     square(pointX, pointY, 10)
@@ -55,42 +52,40 @@ function draw() {
     } else if (keyIsDown(RIGHT_ARROW) && !(playerX + playerSize >= 400)) {
       playerX += playerSpeed;
     }
-    /*
-      
-      Timer // I'll try to make this part clearer
 
-    */
+
+    /*Timer */
+
+    if (didPlayerMove() === true && gameOver() === false) {
+      time -= 1 / 60
+    }
 
 
 
     //Print the score and the timer to the screen 
     fill(255)
     textSize(25)
-    text("score :", 300, 30)
+    text("Score :", 300, 30)
     text("Timer :", 300, 120)
     textSize(50)
     text(score, scorePosition, 90)
-    text(time, timerPosition, 170)
+    text(nf(time, 1, 1), timerPosition, 170)
+
+
 
   } else if (gameIsPlaying === false) {
     clear()
     score = 0;
-    //time = 10;
+    time = 10;
     playerX = 5;
     playerY = 5;
   }
-  didPlayerMove();
-  //gameOver();
 }
 
 
 //Align the timer and score text
 function alignTimerText() {
-  if (time == 10) {
-    timerPosition = 310;
-  } else {
-    timerPosition = 340;
-  }
+  
   if (score < 10) {
     scorePosition = 340;
   } else {
@@ -102,6 +97,7 @@ function alignTimerText() {
 // Detect if the player is on the point and changes his place if true
 function IsPlayerTouching() {
   if (playerX + playerSize + playerSpeed > pointX && playerX + playerSpeed < pointX + 10 && playerY + 20 > pointY && playerY < pointY + 10) {
+
     // If the points' X (RdX) is on the score text, make so RdY is not in it too
     let RdX = random(0, 330);
     pointX = RdX;
@@ -121,20 +117,16 @@ function IsPlayerTouching() {
 function didPlayerMove() {
   if (playerX == 5 && playerY == 5 && time == 10) {
     time = 10
+  } else {
     return true;
   }
 }
 //Reset the score if time = 0
 function gameOver() {
-  if (time == 0) {
-    score = 0
-    //return true;
+  if (time <= 0) {
+    score = 0;
+    timer = 0;
+    return true;
   }
-}
-
-function countDown() {
-  if (didPlayerMove() === true /*&& gameIsPlaying === true*/) {
-    time--
-
-  }
+  return false;
 }
